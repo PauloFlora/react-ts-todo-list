0f6907d1ca11905e368d54667eb3480be8340ced
paulo <tflora.2004@gmail.com>
2022-05-19 05:06:18 -0300

    feat:  integrated app with API and created sort functions.

--- src/pages/ToDoList.tsx
+++ src/pages/ToDoList.tsx
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+interface ITask {
+  id: number,
+  task: string,
+  status: string,
+}
 
 export default function ToDoList() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<ITask[]>([]);
   const [taskInput, setTaskInputt] = useState('');
   const [isDisabled, setIsDisabled] = useState(true);
 
+  // fetch functions
+  const getTasks = useCallback(async (): Promise<void> => {
+    const resposne: Response = await fetch('http://localhost:3001/tasks');
+    const newTasks = await resposne.json();
+    setTasks(newTasks);
+  }, []);  
+
+  // Sorting functions
+  function sortAToZ() {
+    const sortedTasks = tasks.sort((a: ITask, b:ITask) => {
+      if(a.task < b.task) return -1;
+      if(b.task < a.task) return 1;
+      return 0;
+    });
+    setTasks([...sortedTasks]);
+  }
+
+  function sortByDate() {
+    const sortedTasks = tasks.sort((a: ITask, b:ITask) => {
+      if(a.id < b.id) return -1;
+      if(b.id < a.id) return 1;
+      return 0;
+    });
+    setTasks([...sortedTasks]);
+  }
+
+  //validations 
   function checkInputValue(input: string) {
     if (input.length < 3) return setIsDisabled(true);
     return setIsDisabled(false);
   }
 
-  function handleSubmit(event: React.SyntheticEvent): void {
-    event.preventDefault();
-    setTasks([...tasks, taskInput]);
-    setTaskInputt('');
-    setIsDisabled(true);
-  }
-
+  // handlers 
   function handleChange(event: React.SyntheticEvent): void {
     const { target } = event;
     const newTask = (target as HTMLInputElement).value;
@@ -24,6 +51,23 @@ export default function ToDoList() {
     setTaskInputt(newTask);
   }
 
+  async function handleSubmit(event: React.SyntheticEvent): Promise<void> {
+    event.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ newTask: taskInput })
+    };
+    await fetch('http://localhost:3001/tasks', requestOptions);
+    await getTasks();
+    setTaskInputt('');
+    setIsDisabled(false);
+  }
+
+  //useEffects
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
     <div>
@@ -42,8 +86,12 @@ export default function ToDoList() {
       </form>
       <div>
         <p> Filtrar por: </p>
-        <button> A - Z </button>
-        <button> Criado em </button>
+        <button
+          onClick={ sortAToZ }
+        > A - Z </button>
+        <button
+          onClick={ sortByDate }
+        > Criado em </button>
         <button> Status </button>
       </div>
       <div>
@@ -51,8 +99,10 @@ export default function ToDoList() {
         <ul>
           { tasks ? tasks.map((task) => {
             return (
-              <li key={ task  }>
-                { task }
+              <li key={ task.id  }>
+                <p> { task.task } </p>
+                <button> { task.status } </button>
+                <button> X </button>
               </li>
             );
           }) : 'Carregando...' }
