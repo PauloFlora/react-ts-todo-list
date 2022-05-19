0e2c5543d172ce9161bd0b48b7c1eced5908e738
paulo <tflora.2004@gmail.com>
2022-05-19 04:32:30 -0300

    feat: tasks can now be added to the task list.

--- src/pages/ToDoList.tsx
+++ src/pages/ToDoList.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ToDoList() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [taskInput, setTaskInputt] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  function checkInputValue(input: string) {
+    if (input.length < 3) return setIsDisabled(true);
+    return setIsDisabled(false);
+  }
+
+  function handleSubmit(event: React.SyntheticEvent): void {
+    event.preventDefault();
+    setTasks([...tasks, taskInput]);
+  }
+
+  function handleChange(event: React.SyntheticEvent): void {
+    const { target } = event;
+    const newTask = (target as HTMLInputElement).value;
+    checkInputValue(newTask);
+    setTaskInputt(newTask);
+  }
+
+
   return (
     <div>
-      <form>
-        <input />
-        <button> Enviar </button>
+      <form
+        onSubmit={ handleSubmit }
+      >
+        <input
+          onChange={ handleChange }
+          type="text"placeholder="Nova Tarefa"
+          value={ taskInput }
+        />
+        <button
+          disabled={ isDisabled }
+          name="taskButton"
+        > Enviar </button>
       </form>
       <div>
         <p> Filtrar por: </p>
@@ -16,7 +47,13 @@ export default function ToDoList() {
       <div>
         <h1> Tarefas </h1>
         <ul>
-          <li> 1. Teste </li>
+          { tasks ? tasks.map((task) => {
+            return (
+              <li key={ task  }>
+                { task }
+              </li>
+            );
+          }) : 'Carregando...' }
         </ul>
       </div>
     </div>
